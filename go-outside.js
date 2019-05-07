fdf7f4c4f844ca30820a034d72e1e098590a903d
Ryan <rsmsnot@outlook.com>
2019-05-07 21:30:09 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,20 @@
+var temperature = -1;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+
+  // Below 0 = scarf weather //
+
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+
+  // Between 0 and 15 = no short sleeves //
+
+} else {
+  console.log("Short sleeves are fine.");
+
+  // Above 15 = short sleeves are fine //
+}
+
+
+console.log("Now you're ready to go outside!");
---
+++ how-many-hundreds.js
@@ -0,0 +1,3 @@
+function howManyHundreds(num) {
+  return parseInt(num / 100)
+}
